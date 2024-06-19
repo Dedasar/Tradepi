1790aca051bee7322b675bdb5c0dbed986b9637e
Dani <denisedanielasartori@gmail.com>
2024-06-19 20:46:01 -0300

    Exclusão de intens desnecessários

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import {BrouwerRouter, Routes, Route} from "react-router-deom";
+
+
+export default function AppRoutes() {
+
+    return (
+        <BrouwerRouter>
+        <Routes>
+        <Route patth='' element={""}> </Route>
+        <Route patth='' element={""}> </Route>
+        <Route patth='' element={""}> </Route>
+        <Route patth='' element={""}> </Route>
+        </Routes>
+        </BrouwerRouter>
+    )
+}
