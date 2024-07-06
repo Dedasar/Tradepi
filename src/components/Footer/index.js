5325831accc7d2ef20563ab658cab5810fe1201d
Dani <denisedanielasartori@gmail.com>
2024-07-06 15:28:46 -0300

    PageLogin

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,25 +1,22 @@
 import styles from "./Footer.module.css"
-import {FaFacebook,FaInstagram,FaLinkedin,FaTwitter} from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
 
-function Footer () {
+function Footer() {
     return (
-    <footer className= {styles.footer} >
-        
-        <div className={styles.sociais}>
-        <a href="#"><FaFacebook /></a>
-        <a href="#"><FaInstagram /></a>
-        <a href="#"><FaLinkedin /></a>
-        <a href="#"><FaTwitter /></a>
-        </div>    
-        <>
-        <h5>PlayTrade &copy; Desenvolvido por GRUPO 22 PI</h5>
-        
-        </>
-    </footer>
+        <footer className={styles.footer} >
+
+            <div className={styles.sociais}>
+                <a href="#"><FaFacebook /></a>
+                <a href="#"><FaInstagram /></a>
+                <a href="#"><FaLinkedin /></a>
+                <a href="#"><FaTwitter /></a>
+            </div>
+            <>
+                <h5>PlayTrade &copy; Desenvolvido por GRUPO 22 PI</h5>
+
+            </>
+        </footer>
     );
-    
-    }
-    
-    
-    
-    export default Footer;
+}
+
+export default Footer;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,53 @@
+import React, { useState } from 'react';
+import styles from './Login.module.css';
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import Container from '../../components/Container';
+import Banner from '../../components/Banner';
 
-function Login() {
-    var texto = "Login"
-    return (texto      
-    );    
-}
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Email:', email);
+        console.log('Password:', password);
+    };
+
+    return (
+        <>
+            <Header />
+            <Container>
+            <Banner imagem="mk"/>
+                <div className={styles.loginForm}>
+                    <h2>Insira seus dados para o Login</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className={styles.inputGroup}>
+                            <label>Email:</label>
+                            <input
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className={styles.inputGroup}>
+                            <label>Password:</label>
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+            </Container>
+            <Footer />
+        </>
+    );
+};
 
 export default Login;
