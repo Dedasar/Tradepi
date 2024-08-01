d14b49705b8d13aa63891d4b3b394749a37c745d
Dani <denisedanielasartori@gmail.com>
2024-08-01 19:52:45 -0300

    Refatoração

--- src/app/repositories/JogosRepository.js
+++ src/app/repositories/JogosRepository.js
@@ -1,60 +1,29 @@
-import conexao from "../database/conexao.js";
+import conexao, { consulta } from "../database/conexao.js";
 
 class JogosRepository {
     create(jogo){
         const sql = "INSERT INTO jogos SET ?"
-        return new Promise((resolve,reject) => {
-            conexao.query(sql, jogo, (erro, resultado) => {
-                if(erro) return reject ('Não foi possível cadastrar')
-                const row = JSON.parse(JSON.stringify(resultado))    
-                return resolve(row)
-            })
-        })
+        return consulta(sql, jogo, 'Não foi possível cadastrar')
     }
     
    findAll(){
         const sql = "SELECT * FROM jogos;"
-        return new Promise((resolve,reject) => {
-            conexao.query(sql, (erro, resultado) => {
-                if(erro) return reject ('Não foi possível localizar')
-                const row = JSON.parse(JSON.stringify(resultado))    
-                return resolve(row)
-            })
-        })
+        return consulta(sql, 'Não foi possível localizar')
     }
 
     findById(id){
         const sql = "SELECT * FROM jogos WHERE id=?;"
-        return new Promise((resolve,reject) => {
-            conexao.query(sql, id, (erro, resultado) => {
-                if(erro) return reject ('Não foi possível localizar')
-                const row = JSON.parse(JSON.stringify(resultado))    
-                return resolve(row)
-            })
-        })
+        return consulta(sql, id, 'Não foi possível localizar')
     }
 
     update(jogo, id){
         const sql = "UPDATE jogos SET ? WHERE id=?;"
-        return new Promise((resolve,reject) => {
-        conexao.query(sql, [jogo, id], (erro, resultado) => {
-            if(erro) return reject ('Não foi possível atualizar')
-                const row = JSON.parse(JSON.stringify(resultado))    
-                return resolve(row)
-            })
-        })
-        
+        return consulta(sql, [jogo, id], 'Não foi possível atualizar')        
     }
 
     delete(id){
         const sql = "DELETE FROM jogos WHERE id=?;"
-        return new Promise((resolve,reject) => {
-            conexao.query(sql, id, (erro, resultado) => {
-                if(erro) return reject ('Não foi possível apagar')
-                const row = JSON.parse(JSON.stringify(resultado))    
-                return resolve(row)
-            })
-        })
+        return consulta(sql, id, 'Não foi possível apagar')        
     }
 }
 
