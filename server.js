febeec25f44c381891de8d10a50ec900398b2046
Dani <denisedanielasartori@gmail.com>
2024-08-01 17:50:26 -0300

    Primeiros passos para conectar Mysql

---
+++ server.js
@@ -0,0 +1,19 @@
+import jogosController from "./src/app/controllers/jogosController";
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/jogos',jogosController.index)
+app.get('/jogos',jogosController.show)
+app.post('/jogos',jogosController.store)
+app.put('/jogos',jogosController.update)
+app.delete('/jogos',jogosController.delete)
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
+})
+
+export default app;
